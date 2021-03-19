faf259fcb0dee238c710153b68e69a75f436415f
tizianaamica <tizianaamicarella.tag@gmail.com>
2021-03-18 23:23:16 -0400

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,8 +43,19 @@ const routes: Routes = [
       ),
   },
   // { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-  { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterModule) },
+  { 
+    path: 'login', 
+    loadChildren: () => 
+    import('./pages/auth/login/login.module').then(
+      m => m.LoginModule
+    ), 
+  },
+  { path: 'register', 
+  loadChildren: () => 
+  import('./pages/auth/register/register.module').then(
+    m => m.RegisterModule
+  ),
+ },
 ];
 
 @NgModule({
